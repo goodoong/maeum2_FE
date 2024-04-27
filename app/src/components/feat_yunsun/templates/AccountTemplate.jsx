744523de12f374794269fba2ddb63f82d7e6a277
ddookddook <yunsun4830@gmail.com>
2024-04-27 21:37:10 +0900

    feat:TutorialScreen 생성, 구현 AccountBtnBox 수정
    TutorialScreen, AccountBtnBox
    
    Relatedto:#9, #6

--- app/src/components/feat_yunsun/templates/AccountTemplate.jsx
+++ app/src/components/feat_yunsun/templates/AccountTemplate.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import Container from "../../common/atom/Container";
 import CustomTitle from "../../common/atom/CustomTitle";
 import CustomText from "../../common/atom/CustomText";
-import AccountBtnBox from "../organism/AccountBtnbox";
+import AccountBtnBox from "../organism/AccountBtnBox";
 
-const AccountTemplate = () =>{
+const AccountTemplate = ({navigation}) =>{
+ 
 
     return (
-        <>
+        
         <Container>
           <CustomTitle>마음의 창</CustomTitle>
           <CustomText size="sm" color="darkgray">연동할 계정을 선택해주세요</CustomText>
-          <AccountBtnBox/>
+          <AccountBtnBox navigation={navigation} />
         </Container>
-        </>
+        
     )
 }
 
--- app/src/screens/Auth/AccountScreen.js
+++ app/src/screens/Auth/AccountScreen.js
@@ -1,24 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import AccountTemplate from "../../components/feat_yunsun/templates/AccountTemplate";
-import Container from "../../components/common/atom/Container";
-import { TouchableOpacity } from "react-native";
-import { Text } from "react-native";
-const AccountScreen = ({ route, navigation }) => {
-  useEffect(() => {}, []);
 
-  const moveSignupScreen = () => {
-    navigation.push("signup1");
-  };
-
+const AccountScreen = ({navigation }) => {
+  
   return (
-    <>
-    <Container> 
-    <TouchableOpacity onPress={moveSignupScreen}>
-        <Text>회원가입1 페이지로 이동합니다.</Text>
-      </TouchableOpacity>
+    
     <AccountTemplate  navigation={navigation}/>
-      </Container>
-    </>
   );
 };
 export default AccountScreen;
