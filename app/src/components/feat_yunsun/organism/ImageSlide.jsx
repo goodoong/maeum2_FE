b01ca656f5900b7f4df403cea87de836268b2a2b
ddookddook <yunsun4830@gmail.com>
2024-05-01 23:11:40 +0900

    feat:ImageSlide를 CustomSlide로 나눔, 튜토리얼사진
    ImageSlide를 CustomSlide로 나눔, 튜토리얼사진 추가
    
    Relatedto:#9

--- app/src/components/feat_yunsun/organism/ImageSlide.jsx
+++ app/src/components/feat_yunsun/organism/ImageSlide.jsx
@@ -2,7 +2,10 @@ import React, { useRef } from 'react';
 import { View, Image, StyleSheet, TouchableOpacity, Text,Button } from 'react-native';
 import Swiper from 'react-native-swiper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import CustomSlide from '../atom/CustomSlide';
+import { styled } from 'nativewind';
 
+const SlideBox=styled(View)
 const ImageSlide = () => {
     const swiperRef = useRef(null);
 
@@ -17,18 +20,19 @@ const ImageSlide = () => {
       swiperRef.current.scrollBy(1, true);
     }
   };
-  const Slide = ({ uri }) => (
-    <View style={styles.slide}>
-      <Image source={{ uri }} style={styles.image} />
-    </View>
-  );
+  
   return (
     <View>
-    <Swiper autoplay={false} circleloop={true} ref={swiperRef}>
-      <Slide uri={"https://source.unsplash.com/1024x768/?nature"} />
-      <Slide uri={"https://source.unsplash.com/1024x768/?water"} />
-      <Slide uri={ "https://source.unsplash.com/1024x768/?girl"} />
+     <SlideBox className='flex-1 justify-center'>
+      <Swiper autoplay={false} circleloop={true} ref={swiperRef}>
+            <CustomSlide source={require('./../../../assets/Images/Tutorial1.png')} text="음성과 카메라 접근에 허용해주세요!" />
+            <CustomSlide source={require('./../../../assets/Images/Tutorial2.png')} text="똑똑이와 스무고개를 시작해보세요!" />
+            <CustomSlide source={require('./../../../assets/Images/Tutorial3.png')} text="원하는 순서와 주제를 고를땐아래의 버튼을 클릭해요"/>
+            <CustomSlide source={require('./../../../assets/Images/Tutorial4.png')} text="게임을 할 땐 마이크 버튼을 꾹 누르고 이야기 해보세요!" />
+            <CustomSlide source={require('./../../../assets/Images/Tutorial5.png')} text="똑똑이와 눈을 마주치면서 대화해보세요!" />
+            <CustomSlide source={require('./../../../assets/Images/Tutorial6.png')} text="질문 횟수 설명 정답이면 버튼을 눌러줘" />
     </Swiper>
+    </SlideBox>
     <View style={styles.buttonContainer}>
     <Icon name='arrow-circle-left' size={50} color='#FFA8BA' onPress={goToPreviousSlide} />
     <Icon name='arrow-circle-right' size={50} color='#FFA8BA' onPress={goToNextSlide} /> 
@@ -46,11 +50,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'center',
-  },
+ 
   buttonContainer: {
     flex: 0.5,
     flexDirection: 'row',
