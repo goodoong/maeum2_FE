555e03fbe729ab095d54fe9e0a7c37c1971794fa
chaemina <chaemina82@naver.com>
2024-05-05 13:58:20 +0900

    fix : Radio Btn css

--- app/src/components/common/atom/RadioButton.jsx
+++ app/src/components/common/atom/RadioButton.jsx
@@ -1,39 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, TouchableOpacity} from 'react-native';
 import CustomText from './CustomText';
+import {styled} from 'nativewind';
+import {scale, moderateScale} from '../../../utils/Scale';
 
-const RadioButton = ({ options, onChange, initialValue }) => {
+const ButtonGroup = styled(View);
+const SingleButtonSet = styled(View);
+const SingleButton = styled(View);
+
+const RadioButton = ({options, onChange, initialValue}) => {
   const [selectedOption, setSelectedOption] = useState('');
 
   useEffect(() => {
     setSelectedOption(initialValue);
   }, [initialValue]);
 
-  const handleOptionChange = (option) => {
+  const handleOptionChange = option => {
     setSelectedOption(option);
     onChange(option);
   };
 
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <ButtonGroup className="flex flex-row items-center space-x-20">
       {options.map((option, index) => (
-        <TouchableOpacity key={index} onPress={() => handleOptionChange(option)}>
-          <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 16 }}>
-            <View
+        <TouchableOpacity
+          key={index}
+          onPress={() => handleOptionChange(option)}>
+          <SingleButtonSet className="flex flex-row items-center">
+            <SingleButton
+              className="rounded-xl border-2 border-buttonyellow"
               style={{
-                width: 24,
-                height: 24,
-                borderRadius: 12,
-                borderWidth: 2,
-                marginRight: 8,
-                borderColor: selectedOption === option ? 'blue' : 'black',
+                width: moderateScale(24, 0.4),
+                height: moderateScale(24, 0.4),
+                marginRight: scale(8),
+                backgroundColor: selectedOption === option ? '#faae2b' : null,
               }}
             />
             <CustomText>{option}</CustomText>
-          </View>
+          </SingleButtonSet>
         </TouchableOpacity>
       ))}
-    </View>
+    </ButtonGroup>
   );
 };
 
--- app/src/components/feat_mina/templates/InformationFixtemplate.jsx
+++ app/src/components/feat_mina/templates/InformationFixtemplate.jsx
@@ -1,43 +1,46 @@
-import React from "react";
-import { View } from "react-native";
-import CustomInput from "../../common/atom/CustomInput";
-import CustomText from "../../common/atom/CustomText";
-import InformationForm from "../organism/InformationForm";
-import RadioButton from "../../common/atom/RadioButton";
-import { scale } from "../../../utils/Scale";
-import { kidInformationData } from "../mocks/mockdata";
-import { guardianInformationData } from "../mocks/mockdata";
+import React from 'react';
+import {View} from 'react-native';
+import CustomInput from '../../common/atom/CustomInput';
+import CustomText from '../../common/atom/CustomText';
+import InformationForm from '../organism/InformationForm';
+import RadioButton from '../../common/atom/RadioButton';
+import {scale} from '../../../utils/Scale';
+import {kidInformationData} from '../mocks/mockdata';
+import {guardianInformationData} from '../mocks/mockdata';
 
-const renderItem = ({ item }) => {
-  if (item.key === "아이 정보" || item.key === "보호자 정보") {
+const renderItem = ({item}) => {
+  if (item.key === '아이 정보' || item.key === '보호자 정보') {
     return (
       <View
         className="w-full flex-row"
-        style={{ padding: scale(12), marginBottom: scale(6), backgroundColor: item.color }}
-      >
+        style={{
+          padding: scale(12),
+          marginBottom: scale(6),
+          backgroundColor: item.color,
+        }}>
         <CustomText>{item.key}</CustomText>
       </View>
-    )
-  } else if (item.key === "성별") {
-    console.log(item.data)
+    );
+  } else if (item.key === '성별') {
+    console.log(item.data);
     return (
       <View
         className="w-full flex-row justify-center items-center"
-        style={{ padding: scale(12), marginBottom: scale(6), backgroundColor: item.color }}
-      >
-        <CustomText>{item.key}</CustomText>
+        style={{
+          padding: scale(12),
+          marginBottom: scale(6),
+          backgroundColor: item.color,
+        }}>
         <RadioButton
-          options={["남", "여"]} // 라디오 버튼 옵션 설정
-          onChange={(selectedOption) => console.log(selectedOption)} // 선택된 옵션 변경 핸들러
+          options={['남', '여']} // 라디오 버튼 옵션 설정
+          onChange={selectedOption => console.log(selectedOption)} // 선택된 옵션 변경 핸들러
           initialValue={item.data}
         />
       </View>
     );
   } else {
     return (
-      <View
-        className="w-full flex-row justify-center items-center"
-      >
+      <View className="w-full flex-row justify-center items-center">
         <CustomInput
           keyboardType="default"
           placeholder={item.key}
@@ -48,17 +51,16 @@ const renderItem = ({ item }) => {
   }
 };
 
-const InformationFixtemplate = ({ navigation }) => {
-
-  //  API 연결 시 핸들러 수정 
+const InformationFixtemplate = ({navigation}) => {
+  //  API 연결 시 핸들러 수정
   const moveInformationScreen = () => {
-    navigation.navigate("info");
+    navigation.navigate('info');
   };
 
   return (
     <InformationForm
       navigation={navigation}
-      data={{ kidInformationData, guardianInformationData }}
+      data={{kidInformationData, guardianInformationData}}
       moveScreen={moveInformationScreen}
       isFix={false}
       renderItem={renderItem}
