f24e38b65d0a185269e4ed1dec2118b9e813076d
chaemina <chaemina82@naver.com>
2024-05-13 04:56:50 +0900

    feat: input validation
    - Auth Form 모듈화, 유효성 검사
    - useForm으로 data 객체 전송
    = CSS 수정
    - 전화번호 입력 시 숫자만 입력

--- app/src/components/feat_yunsun/templates/AuthorizationScreen.jsx
+++ app/src/components/feat_yunsun/templates/AuthorizationScreen.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
-import { Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import AuthorizationForm from "../organism/AuthorizationForm";
 import Container from "../../common/atom/Container";
-import CustomBtn from "../../common/atom/CustomBtn";
 import CustomText from "../../common/atom/CustomText";
-import CustomInput from "../../common/atom/CustomInput";
 import CustomTitle from "../../common/atom/CustomTitle";
 import { styled } from 'nativewind';
 
@@ -16,25 +13,18 @@ const AuthorizationTemplate = ({ route, navigation }) => {
 
     }, []);
 
-    const moveSignup2Screen = () => {
-        navigation.push('signup2');
-    }
+    const onSubmit = data => {
+        console.log(data); 
+        navigation.navigate('signup2');
+      };
 
     return (
         <Box className="space-y-4"> 
             <CustomTitle>전화번호 인증</CustomTitle>
-            <CustomText size='sm' color=''>(전화번호)로 온 번호 네 자리를 입력해주세요</CustomText>
-            <CustomInput keyboardType="numeric"  placeholder="인증번호" />
-            <TouchableOpacity>
-                <Text>인증번호 다시 받기</Text>
-            </TouchableOpacity>
-            <CustomBtn 
-             size='lg'
-             color='buttonyellow'
-             rounded= {true}
-             title="다음"
-             onPress={moveSignup2Screen}
-            />
+            {/* 전화번호 값 잠시 전역 변수로 저장해놓고 출력하기 */}
+            <CustomText size='sm' color=''>(전화번호)로 온 숫자 네 자리를 입력해주세요</CustomText>
+            <AuthorizationForm 
+             onSubmit={onSubmit}/>
         </Box>
     )
 
