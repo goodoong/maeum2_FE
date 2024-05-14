15b58f0bd15ba35e17c50a533de40ff0243d8bd1
ddookddook <yunsun4830@gmail.com>
2024-05-14 12:18:35 +0900

    feat:tutorial 화면 수정
    text 잘림 수정, ScrollView 적용
    tablet 이미지 잘림 수정 필요
    
    Relatedto:#9

--- app/src/components/feat_yunsun/organism/ImageSlide.jsx
+++ app/src/components/feat_yunsun/organism/ImageSlide.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { View, StyleSheet} from 'react-native';
+import { View, StyleSheet, ScrollView} from 'react-native';
 import Swiper from 'react-native-swiper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import CustomSlide from '../atom/CustomSlide';
@@ -42,37 +42,25 @@ const ImageSlide = ({route, navigation}) => {
   }
   };
   return (
-    <View>
-      <SlideBox className='basis-2/3 justify-center'>
+    <ScrollView>
+    <View className='flex flex-col space-y-20'>
+      <SlideBox className='basis-3/4 justify-center space-y-10'>
         <Swiper autoplay={false} loop={true} ref={swiperRef} onIndexChanged={onIndexChanged}>
           <CustomSlide source={require('./../../../assets/Images/Tutorial1.png')} text="음성과 카메라 접근에 허용해주세요!" />
           <CustomSlide source={require('./../../../assets/Images/Tutorial2.png')} text="똑똑이와 스무고개를 시작해보세요!" />
           <CustomSlide source={require('./../../../assets/Images/Tutorial3.png')} text="원하는 순서와 주제를 고를땐아래의 버튼을 클릭해요" />
           <CustomSlide source={require('./../../../assets/Images/Tutorial4.png')} text="게임을 할 땐 마이크 버튼을 꾹 누르고 이야기 해보세요!" />
           <CustomSlide source={require('./../../../assets/Images/Tutorial5.png')} text="똑똑이와 눈을 마주치면서 대화해보세요!" />
-          <CustomSlide source={require('./../../../assets/Images/Tutorial6.png')} text="질문 횟수 설명 정답이면 버튼을 눌러줘" />
+          <CustomSlide source={require('./../../../assets/Images/Tutorial6.png')} text="질문 횟수 설명 정답이면 버튼을 눌러주세요!" />
         </Swiper>
       </SlideBox>
-      <View style={styles.buttonContainer}>
+      <View className='flex flex-row basis-1/4 space-y-5 justify-between'>
         <Icon name='arrow-circle-left' size={50} color='#FFA8BA' onPress={goToPreviousSlide} />
         <ChangeBtn/>
       </View>
     </View>
+    </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  
-  buttonContainer: {
-    flex: 0.5,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: '5%',
-  },
-  currentIndexText: {
-    textAlign: 'center',
-    marginTop: 10,
-  },
-});
 
 export default ImageSlide;
