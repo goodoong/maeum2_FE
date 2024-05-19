745e2413fee5742c1ebf33b400b4af5f02d2af8a
chaemina <chaemina82@naver.com>
2024-05-19 19:09:59 +0900

    fix : config.js
    reactoron에서 console 로깅

--- app/config.js
+++ app/config.js
@@ -3,3 +3,13 @@ import Reactotron from 'reactotron-react-native';
 Reactotron.configure() // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
+
+const yeOldeConsoleLog = console.log;
+console.log = (...args) => {
+  yeOldeConsoleLog(...args);
+  Reactotron.display({
+    name: 'CONSOLE.LOG',
+    value: args,
+    preview: args.length > 0 && typeof args[0] === 'string' ? args[0] : null,
+  });
+};
