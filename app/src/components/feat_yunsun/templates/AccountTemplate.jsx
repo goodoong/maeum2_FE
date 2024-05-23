910a6e80ad05c13424bbda35de88bb57817bf28c
chaemina <chaemina82@naver.com>
2024-05-24 02:29:30 +0900

    fix

--- app/src/components/feat_yunsun/templates/AccountTemplate.jsx
+++ app/src/components/feat_yunsun/templates/AccountTemplate.jsx
@@ -1,34 +1,21 @@
-import React from "react";
-import ScrollContainer from "../../common/atom/ScrollContainer";
-import CustomTitle from "../../common/atom/CustomTitle";
-import CustomText from "../../common/atom/CustomText";
-import AccountBtnBox from "../organism/AccountBtnbox";
-import { styled } from "nativewind";
-import { useQuery, QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { testAPI } from "../../../service/user"; // testAPI를 불러옵니다.
+import React from 'react';
+import ScrollContainer from '../../common/atom/ScrollContainer';
+import CustomTitle from '../../common/atom/CustomTitle';
+import CustomText from '../../common/atom/CustomText';
+import AccountBtnBox from '../organism/AccountBtnbox';
+import {styled} from 'nativewind';
+import {useQuery} from '@tanstack/react-query';
+import {testAPI} from '../../../service/user'; // testAPI를 불러옵니다.
 
 const Box = styled(ScrollContainer);
 
-const queryClient = new QueryClient();
-
-const AccountTemplate = ({ navigation }) => {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ['testData'],
-    queryFn: testAPI
-  });
-
-  if (isLoading) {
-    return <CustomText>Loading...</CustomText>;
-  }
-
-  if (error) {
-    return <CustomText>Error: {error.message}</CustomText>;
-  }
-
+const AccountTemplate = ({navigation}) => {
   return (
     <Box className="space-y-4">
       <CustomTitle>마음의 창</CustomTitle>
-      <CustomText size="sm" color="darkgray">연동할 계정을 선택해주세요</CustomText>
+      <CustomText size="sm" color="darkgray">
+        연동할 계정을 선택해주세요
+      </CustomText>
       <AccountBtnBox navigation={navigation} />
     </Box>
   );
--- app/src/service/user.js
+++ app/src/service/user.js
@@ -1,6 +1,12 @@
-import { instance } from "./instance";
+import {instance} from './instance';
 
 export const testAPI = async () => {
-  const response = await instance.get("/api/test");
+  const response = await instance.get('/api/test');
+  return response.data;
+};
+
+// 카카오에서 발급 받은 인가코드를 담아서 post
+export const kakaoapi = async () => {
+  const response = await instance.post('/api/login/kakao');
   return response.data;
 };
