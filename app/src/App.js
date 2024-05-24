ecbde2aec036cf514a64860b00544209f70862a8
chaemina <chaemina82@naver.com>
2024-05-25 03:30:01 +0900

    chore : redux toolkit

--- app/src/App.js
+++ app/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import StackNavigation from './navigation/StackNavigation';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {Provider} from 'react-redux';
-import store from './redux/store';
+
 
 /**
  * Main App
@@ -18,12 +17,10 @@ const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <Provider store={store}>
       <QueryClientProvider client={queryClient}>
        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <StackNavigation />
       </QueryClientProvider>
-    </Provider>
   );
 };
 export default App;
