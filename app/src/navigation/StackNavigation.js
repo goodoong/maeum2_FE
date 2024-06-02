43806caae954dee90fde277fdb3b8ec09300d1b2
ddookddook <yunsun4830@gmail.com>
2024-06-02 19:33:48 +0900

    fix: charactername 캐릭터 얼굴 수정
    charactername 캐릭터 얼굴 위치 수정

--- app/src/navigation/StackNavigation.js
+++ app/src/navigation/StackNavigation.js
@@ -43,7 +43,7 @@ const StackNavigation = () => {
   return (
     <NavigationContainer theme={navTheme}>
       <Stack.Navigator
-        initialRouteName={'main'}
+        initialRouteName={'charactername'}
         screenOptions={({route}) => ({
           ...customStackNavigationOptions,
           headerShown: !(
