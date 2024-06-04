d1ec380f9d3019d6de1097827a5056a47757c58b
chaemina <chaemina82@naver.com>
2024-06-04 13:52:30 +0900

    feat: home api 연결
    - main home 화면에서 사용자의 이름과 메세지를 출력함

--- app/src/components/feat_mina/organism/Mainorganism.jsx
+++ app/src/components/feat_mina/organism/Mainorganism.jsx
@@ -4,18 +4,41 @@ import CustomTitle from '../../common/atom/CustomTitle';
 import CustomText from '../../common/atom/CustomText';
 import {styled} from 'nativewind';
 import {scale} from '../../../utils/Scale';
+import {useQuery} from '@tanstack/react-query';
+import {mainapi} from '../../../service/main';
+import Loading from '../../common/atom/Loading';
 
-// styled 함수를 사용하여 View를 스타일링한 컴포넌트 생성
 const Body = styled(View);
 
 const MainOrganism = () => {
+  const {data, error, isLoading} = useQuery({
+    queryKey: ['mainapi'],
+    queryFn: mainapi,
+  });
+
+  if (isLoading) {
+    return <Loading width={100} height={100} loop={true} />;
+  }
+
+  if (error) {
+    return (
+      <Body
+        className="w-full flex-col space-y-4"
+        style={{paddingLeft: scale(20)}}>
+        <CustomText>Error loading data</CustomText>
+      </Body>
+    );
+  }
+
+  const {child_first_name, message} = data.response;
+
   return (
     <Body
       className="w-full flex-col space-y-4"
       style={{paddingLeft: scale(20)}}>
       {/* 메시지 값을 서버에서 전달 받아서 출력 */}
-      <CustomTitle color="pink">마음아</CustomTitle>
-      <CustomText>오늘 하루도 분명 재밌을거야!</CustomText> 
+      <CustomTitle color="pink">{child_first_name}(이)아!</CustomTitle>
+      <CustomText>{message}</CustomText>
       {/* 이건 디폴트 */}
       <CustomText>아래 버튼을 눌러 게임을 시작해봐</CustomText>
     </Body>
