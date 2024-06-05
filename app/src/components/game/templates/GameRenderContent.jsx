7b785a639244e8898c34419b0cf21c6f335d2a5d
chaemina <chaemina82@naver.com>
2024-06-06 02:59:31 +0900

    feat: game end
    - 게임 종료 시 그만할래요 혹은 계속할래요 처리

--- app/src/components/game/templates/GameRenderContent.jsx
+++ app/src/components/game/templates/GameRenderContent.jsx
@@ -6,7 +6,7 @@ import useTTS from '../../../hooks/useTTS';
 import STTScreen from '../../../screens/STTScreen';
 import useGameTurn from '../../../hooks/useGameTurn';
 import useGameResult from '../../../hooks/useGameResult';
-
+import CustomModal from '../../common/atom/CustomModal';
 
 const GameRenderContent = ({ navigation }) => {
   const route = useRoute();
@@ -14,6 +14,8 @@ const GameRenderContent = ({ navigation }) => {
   const [subtitle, setSubtitle] = useState('');
   const [feelingStatus, setFeelingStatus] = useState('default');
   const [initialMessageSpoken, setInitialMessageSpoken] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false); 
+  const [modalContent, setModalContent] = useState(''); 
   const { loading: ttsLoading, handleTTS } = useTTS();
   const { sendRequest } = useGameTurn(); 
   const { gameWinMutation, gameLoseMutation } = useGameResult();
@@ -40,8 +42,12 @@ const GameRenderContent = ({ navigation }) => {
 
       if (isSolved && isEnd) {
         gameWinMutation.mutate(); // 성공 API 호출
+        setModalContent('게임을 성공적으로 완료했습니다. 계속하시겠습니까?');
+        setModalVisible(true); // 모달 보이기
       } else if (!isSolved && isEnd) {
         gameLoseMutation.mutate(); // 실패 API 호출
+        setModalContent('게임이 실패했습니다. 계속하시겠습니까?');
+        setModalVisible(true); // 모달 보이기
       }
     },
     onError: (error) => {
@@ -55,18 +61,43 @@ const GameRenderContent = ({ navigation }) => {
     mutation.mutate(spokenText);
   };
 
+  const handleConfirm = () => {
+    setModalVisible(false);
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'selectscreen' }],
+    });
+  };
+
+  const handleCancel = () => {
+    setModalVisible(false);
+    navigation.navigate('main');
+  };
+
   const renderContent = () => (
     <STTScreen onSpeechResult={handleSpeechResult} />
   );
 
   return (
-    <GameTemplate
-      subtitleText={subtitle}
-      loading={ttsLoading || mutation.isLoading}
-      renderContent={renderContent}
-      navigation={navigation}
-      feelingData={ttsLoading || mutation.isLoading ? 'talkingmouth' : feelingStatus}
-    />
+    <>
+      <GameTemplate
+        subtitleText={subtitle}
+        loading={ttsLoading || mutation.isLoading}
+        renderContent={renderContent}
+        navigation={navigation}
+        feelingData={ttsLoading || mutation.isLoading ? 'talkingmouth' : feelingStatus}
+      />
+      <CustomModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        title="게임 종료"
+        content={modalContent}
+        confirmText="계속할래요"
+        cancelText="그만할래요"
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
+      />
+    </>
   );
 };
 
