e1eb12105da5ced8d6c6af6a9e3387b67c4c1f28
ddookddook <yunsun4830@gmail.com>
2024-06-06 04:14:31 +0900

    fix:튜토리얼 사진, 글씨 수정
    튜토리얼6 사진 교체
    slide text 줄바꿈
    space-y 적용

--- app/src/components/feat_yunsun/organism/ImageSlide.jsx
+++ app/src/components/feat_yunsun/organism/ImageSlide.jsx
@@ -15,10 +15,10 @@ const CustomContainer = styled(ScrollContainer);
 const slides = [
   { source: require('../../../assets/Images/Tutorial1.png'), text: "음성과 카메라 접근에 허용해주세요!" },
   { source: require('../../../assets/Images/Tutorial2.png'), text: "똑똑이와 스무고개를 시작해보세요!" },
-  { source: require('../../../assets/Images/Tutorial3.png'), text: "원하는 순서와 주제를 고를 땐 아래의 버튼을 클릭해요" },
-  { source: require('../../../assets/Images/Tutorial4.png'), text: "게임을 할 땐 마이크 버튼을 꾹 누르고 이야기 해보세요!" },
+  { source: require('../../../assets/Images/Tutorial3.png'), text: "원하는 순서와 주제를 고를 땐\n 아래의 버튼을 클릭해요" },
+  { source: require('../../../assets/Images/Tutorial4.png'), text: "게임을 할 땐 마이크 버튼을 꾹 누르고\n 이야기 해보세요!" },
   { source: require('../../../assets/Images/Tutorial5.png'), text: "똑똑이와 눈을 마주치면서 대화해보세요!" },
-  { source: require('../../../assets/Images/Tutorial6.png'), text: "질문 횟수 설명 정답이면 버튼을 눌러주세요!" }
+  { source: require('../../../assets/Images/Tutorial6.png'), text: "답을 알겠으면 [정답은 -(이)야!]로,\n질문에는 [맞아],[아니야]로\n대답해주세요!" }
 ];
 const ImageSlide = ({ navigation }) => {
   const scrollViewRef = useRef();
@@ -52,7 +52,7 @@ const ImageSlide = ({ navigation }) => {
       >
         {slides.map((slide, index) => (
           <CustomContainer key={index}>
-            <Slide style={{ width: width, paddingRight: 20 }} className="items-center">
+            <Slide style={{ width: width, paddingRight: 20 }} className="items-center space-y-4">
               <CustomImage source={slide.source} width={moderateScale(350, 0.3)} height={verticalScale(400)} />
               <CustomText size="lg" color="headline" numberOfLines={3}>{slide.text}</CustomText>
             </Slide>
